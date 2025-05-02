96070e57d0a79d8bb1fbf7ec41f28cdbc57ece29
Vicenzo Fonseca de Mello Souza <41163058+vicenzofms@users.noreply.github.com>
2025-05-01 22:49:02 -0300

    fix: tabela sem botoes de ação agora tem padding-y

--- src/front/src/app/components/search-box-multi/search-box-multi.component.ts
+++ src/front/src/app/components/search-box-multi/search-box-multi.component.ts
@@ -1,85 +1,88 @@
-import { Component, Input, Output, EventEmitter, ElementRef, HostListener } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; 
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
+import {
+	Component,
+	Input,
+	Output,
+	EventEmitter,
+	ElementRef,
+	HostListener,
+} from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-search-box-multi',
-  standalone: true,
-  imports: [CommonModule, FormsModule],
-  templateUrl: './search-box-multi.component.html',
-  styleUrl: './search-box-multi.component.css'
+	selector: "app-search-box-multi",
+	standalone: true,
+	imports: [CommonModule, FormsModule],
+	templateUrl: "./search-box-multi.component.html",
+	styleUrl: "./search-box-multi.component.css",
 })
 export class SearchBoxMultiComponent {
-  @Input() label: string = '';
-  @Input() options: any[] = [];
-  @Input() optionLabel: string = '';
-  @Input() optionValue: string = '';
-  @Output() selectionChange = new EventEmitter<any[]>();
-  @Input() nullLabel: string = '';
-  @Output() searchChange = new EventEmitter<string>();
+	@Input() label: string = "";
+	@Input() options: any[] = [];
+	@Input() optionLabel: string = "";
+	@Input() optionValue: string = "";
+	@Output() selectionChange = new EventEmitter<any[]>();
+	@Input() nullLabel: string = "";
+	@Output() searchChange = new EventEmitter<string>();
 
+	selectedValues: any[] = [];
+	showDropdown = false;
+	searchText: string = "";
+	searchTimeout: any;
 
-  selectedValues: any[] = [];
-  showDropdown = false;
-  searchText: string = '';
-  searchTimeout: any;
+	constructor(private elementRef: ElementRef) {}
 
+	getPropByPath(obj: any, path: string): any {
+		return path.split(".").reduce((acc, part) => acc?.[part], obj);
+	}
 
-  constructor(private elementRef: ElementRef) {}
+	toggleDropdown() {
+		this.showDropdown = !this.showDropdown;
+	}
 
-  
-  getPropByPath(obj: any, path: string): any {
-    return path.split('.').reduce((acc, part) => acc?.[part], obj);
-  }
+	isChecked(value: any): boolean {
+		return this.selectedValues.includes(value);
+	}
 
-  toggleDropdown() {
-    this.showDropdown = !this.showDropdown;
-  }
+	onCheckboxChange(option: any) {
+		const value = this.getPropByPath(option, this.optionValue);
+		const index = this.selectedValues.indexOf(value);
 
-  isChecked(value: any): boolean {
-    return this.selectedValues.includes(value);
-  }
+		if (index > -1) {
+			this.selectedValues.splice(index, 1);
+		} else {
+			this.selectedValues.push(value);
+		}
 
-  onCheckboxChange(option: any) {
-    const value = this.getPropByPath(option, this.optionValue); 
-    const index = this.selectedValues.indexOf(value);
-  
-    if (index > -1) {
-      this.selectedValues.splice(index, 1);
-    } else {
-      this.selectedValues.push(value);
-    }
-  
-    this.selectionChange.emit(this.selectedValues);
-  }
-  
-  get selectedLabels(): string[] {
-    return this.options
-      .filter(option => this.selectedValues.includes(this.getPropByPath(option, this.optionValue))) 
-      .map(option => this.getPropByPath(option, this.optionLabel));
-  }
-  
+		this.selectionChange.emit(this.selectedValues);
+	}
 
-  @HostListener('document:click', ['$event'])
-  onClickOutside(event: MouseEvent) {
-    if (!this.elementRef.nativeElement.contains(event.target)) {
-      this.showDropdown = false;
-    }
-  }
+	get selectedLabels(): string[] {
+		return this.options
+			.filter((option) =>
+				this.selectedValues.includes(
+					this.getPropByPath(option, this.optionValue),
+				),
+			)
+			.map((option) => this.getPropByPath(option, this.optionLabel));
+	}
 
-  onSearchChange(value: string) {
-    this.searchText = value;
-  
-    if (this.searchTimeout) {
-      clearTimeout(this.searchTimeout);
-    }
-  
-    this.searchTimeout = setTimeout(() => {
-      this.searchChange.emit(this.searchText.trim());
-    }, 1000); // 1 segundo
-  }
-  
+	@HostListener("document:click", ["$event"])
+	onClickOutside(event: MouseEvent) {
+		if (!this.elementRef.nativeElement.contains(event.target)) {
+			this.showDropdown = false;
+		}
+	}
 
+	onSearchChange(value: string) {
+		this.searchText = value;
+
+		if (this.searchTimeout) {
+			clearTimeout(this.searchTimeout);
+		}
+
+		this.searchTimeout = setTimeout(() => {
+			this.searchChange.emit(this.searchText.trim());
+		}, 1000); // 1 segundo
+	}
 }
