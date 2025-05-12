08d402140585011700d2726fcdfe626810678f64
Ana Coimbra <ana.coimbra.ti@gmail.com>
2025-05-12 17:16:45 -0300

    commit_Ana_01

--- src/front/src/app/pages/Aluno/calendar-aluno-page/calendar-aluno-page.component.ts
+++ src/front/src/app/pages/Aluno/calendar-aluno-page/calendar-aluno-page.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
-
+import { BotaoComponent } from "../../../components/botao/botao.component";
+import { OtherCardComponent } from "../../../components/other-card/other-card.components";
 @Component({
   selector: 'app-calendar-aluno-page',
-  imports: [],
+  imports: [OtherCardComponent,BotaoComponent],
   templateUrl: './calendar-aluno-page.component.html',
   styleUrl: './calendar-aluno-page.component.css'
 })
+
 export class CalendarAlunoPageComponent {
+  meses: string[] = [
+    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+  ];
+
+  mesAtual: number = new Date().getMonth();
+
+  get nomeMesAtual(): string {
+    return this.meses[this.mesAtual];
+  }
+
+  proximoMes(): void {
+    this.mesAtual = (this.mesAtual + 1) % 12;
+  }
+
+  mesAnterior(): void {
+    this.mesAtual = (this.mesAtual - 1 + 12) % 12;
+  }
+
+  
 
 }
+
