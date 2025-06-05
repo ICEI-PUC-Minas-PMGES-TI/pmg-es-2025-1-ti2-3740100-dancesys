113472d4ed18737ee8057048958b89f822c2557a
Henrique Carvalho <henrique.carvalho1220@gmail.com>
2025-06-05 01:37:39 -0300

    Botao Deslogar user-page

--- src/front/src/app/pages/usuario-page/usuario-page.component.ts
+++ src/front/src/app/pages/usuario-page/usuario-page.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BotaoComponent } from '../../components/botao/botao.component';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-usuario-page',
-  imports: [],
+  standalone : true,
+  imports: [BotaoComponent],
   templateUrl: './usuario-page.component.html',
   styleUrl: './usuario-page.component.css'
 })
 
 export class UsuarioPageComponent {
+
+  usuarioService = inject(UsuarioService);
+
+  deslogar() {
+    this.usuarioService.deslogar();
+  }
 
 }
