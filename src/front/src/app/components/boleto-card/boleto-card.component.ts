10786ea5921e14f570d7e6bb4a44723f794d40da
Henrique Carvalho <henrique.carvalho1220@gmail.com>
2025-06-05 00:24:14 -0300

    Componente-Boleto

---
+++ src/front/src/app/components/boleto-card/boleto-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-boleto-card',
+  templateUrl: './boleto-card.component.html',
+  styleUrls: ['./boleto-card.component.css']
+})
+export class BoletoCardComponent {
+  @Input() boleto!: { mesAno: string; valor: number; status: string; };
+
+  getCorBotao(status: string): string {
+  switch (status) {
+    case 'pago': return 'green';
+    case 'atrasado': return 'red';
+    default: return 'brown';
+  }
+}
+
+}
--- src/front/src/app/pages/Aluno/financeiro-aluno-page/financeiro-aluno-page.component.ts
+++ src/front/src/app/pages/Aluno/financeiro-aluno-page/financeiro-aluno-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-financeiro-aluno-page',
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   templateUrl: './financeiro-aluno-page.component.html',
   styleUrl: './financeiro-aluno-page.component.css'
 })
-export class FinanceiroAlunoPageComponent {
+export class FinanceiroAlunoPageComponent implements OnInit {
 
+boletos = [
+    { mesAno: '03/2025', valor: 199.99, status: 'pendente' },
+    { mesAno: '02/2025', valor: 214.99, status: 'atrasado' },
+    { mesAno: '01/2025', valor: 199.99, status: 'pago' }
+  ];
+
+  constructor() {}
+
+  ngOnInit(){}
+ 
 }
