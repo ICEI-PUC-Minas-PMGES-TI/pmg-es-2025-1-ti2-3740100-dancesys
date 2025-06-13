dce6c03a940d1a139d93c9e868b380bec7eb8ad5
Júlia Rocha <juliarochafiorini@gmail.com>
2025-06-13 00:48:35 -0300

    Finalizando tela de eventos-alunos

--- src/front/src/app/pages/Aluno/eventos-aluno-page/eventos-aluno-page.component.ts
+++ src/front/src/app/pages/Aluno/eventos-aluno-page/eventos-aluno-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+import { UsuarioService } from "../../../services/usuario.service";
 import { AdminService } from "../../../services/admin.service";
 import { EventoResponse } from "../../../models/evento.model";
 import { AlertService } from "../../../services/Alert.service";
@@ -8,17 +11,32 @@ import { AlertService } from "../../../services/Alert.service";
 	selector: "app-eventos-aluno-page",
 	templateUrl: "./eventos-aluno-page.component.html",
 	styleUrls: ["./eventos-aluno-page.component.css"],
-  imports: [CommonModule]
+	imports: [CommonModule, ReactiveFormsModule],
 })
 export class EventosAlunoPageComponent implements OnInit {
 	eventos: EventoResponse | undefined;
+	isModalOpen = false;
+	eventoId: number | undefined;
+	alunoId: number | undefined;
+	ingressoForm: FormGroup;
 
 	constructor(
 		private adminService: AdminService,
-		private alertService: AlertService
-	) {}
+		private alertService: AlertService,
+		private fb: FormBuilder,
+		private usuarioService: UsuarioService
+	) {
+		this.ingressoForm = this.fb.group({
+			qtdIngressos: [
+				1,
+				[Validators.required, Validators.min(1), Validators.max(10)],
+			],
+			tipoIngresso: ["normal", Validators.required],
+		});
+	}
 
 	ngOnInit(): void {
+		this.alunoId = this.usuarioService.usuario()?.id;
 		this.onFiltrar();
 	}
 
@@ -40,5 +58,28 @@ export class EventosAlunoPageComponent implements OnInit {
 					this.alertService.erro("Erro ao carregar eventos.");
 				},
 			});
+	}
+
+	comprarIngresso(eventoId: number): void {
+		this.eventoId = eventoId;
+		this.isModalOpen = true;
+	}
+
+	fecharModal(): void {
+		this.isModalOpen = false;
+	}
+
+	processarCompra(): void {
+		if (this.ingressoForm.valid && this.alunoId !== undefined) {
+			const qtdIngressos = this.ingressoForm.value.qtdIngressos;
+			const tipoIngresso = this.ingressoForm.value.tipoIngresso;
+			console.log(
+				`Evento ID: ${this.eventoId}, Aluno ID: ${this.alunoId}, Quantidade: ${qtdIngressos}, Tipo: ${tipoIngresso}`
+			);
+			this.alertService.sucesso("Compra realizada com sucesso!");
+			this.fecharModal();
+		} else {
+			this.alertService.erro("ID do aluno não encontrado.");
+		}
 	}
 }
